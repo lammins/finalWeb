6a6d2db6162da4eddc22d43c64ea7d63a1091d97
lammins <builam2508@gmail.com>
2024-04-10 10:56:38 +0700

    add project

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const port = 3000
 
 var bodyParser = require('body-parser');
 const orderModule = require('./order')
-const { orders } = require('./order');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+const querystring = require('querystring')
+
 const mongodbModule = require("./public/Javascript/mongoDB");
+
 
 mongodbModule.connectToMongoDB()
     .then(() => {
@@ -20,6 +22,11 @@ mongodbModule.connectToMongoDB()
         console.error('Failed to connect to MongoDB:', err);
         server.close();
     });
+
+
+const server = app.listen(3000, () => {
+    console.log('Sever is running on port 3000');
+});
 
 //Listen on shutdown server event(Ctrl+C...)
 process.on('SIGINT', () => {
@@ -35,75 +42,169 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/pages/index.html');
 });
 
-app.get('/order', (req,res) =>
+app.get('/order', async (req,res) =>
 {
     try {
-        //const orders = mongoModule.findOrder()
-        const html = orderModule.output();
+        const orders = await mongodbModule.findOrderList();
+        const html = orderModule.output(orders);
         res.send(html) 
     } catch(err) {
         console.error('Failed to fetch documents', err);
         res.status(500).send('Failed to fetch documents');
     }
-    html = orderModule.output()
-    res.send(html)
 })
 
+app.post('/createManyOrders', async (req, res) => {
+    const order = req.body;
+    console.log(order);
+    mongodbModule.addManyOrders(order);
+    const message  = `${order.length} orders created successfully!`
+    res.json({message: message})
+});
 
-app.post('/api/order', (req, res) => {
+app.get('/read',async (req, res) => {
+    const paymentMethod = req.query.paymentMethod;
+    const minPrice = req.query.minPrice; 
+    const maxPrice = req.query.maxPrice;
+    const query = {};
+
+    if (paymentMethod) {
+        query.paymentMethod = {$regex: paymentMethod, $options: 'i'}
+    }
+    if (minPrice) {
+        query.totalAmount = {$gte: parseFloat(minPrice)};
+    }
+    
+    if (minPrice && maxPrice) {
+        query.totalAmount = {$gte: parseFloat(minPrice), $lte: parseFloat(maxPrice)};
+    } else if (minPrice) {
+        query.totalAmount = {$lte: parseFloat(minPrice)};
+    } else if (maxPrice) {
+        query.totalAmount = {$gte: parseFloat(maxPrice)};
+    }
+
     try {
-        const orders = mongoModule.findOrder()
-        const html = orderModule.output();
-        res.send(html) 
-    } catch(err) {
-        console.error('Failed to fetch documents', err);
-        res.status(500).send('Failed to fetch documents');
+        const searchResult = await mongodbModule.findDocuments(query);
+        const tableRowsHTML = searchResult.map(order => {
+            return `
+                <tr>
+                    <td>${order.orderID}</td>
+                    <td>${order.orderDate}</td>
+                    <td>$${order.totalAmount}</td>
+                    <td>${order.orderStatus}</td>
+                    <td>${order.paymentMethod}</td>
+                    <td><img src="/public/images/${order.image}" alt="" style="max-width: 100px;"></td>
+                </tr>
+            `;
+        }).join('');
+        res.send(`
+            <html>
+            <head>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        margin: 0;
+                        padding: 0;
+                        background-color: #f4f4f4;
+                    }
+                    table {
+                        width: 100%;
+                        border-collapse: collapse;
+                        background-color: #fff;
+                        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+                    }
+                    th, td {
+                        padding: 12px 15px;
+                        text-align: left;
+                    }
+                    th {
+                        background-color: #007bff;
+                        color: #fff;
+                    }
+                    tr:nth-child(even) {
+                        background-color: #f2f2f2;
+                    }
+                    img {
+                        max-width: 100px;
+                        height: auto;
+                        display: block;
+                        margin: 0 auto;
+                    }
+                    .back-icon {
+                        text-align: left;
+                        margin-bottom: 20px;
+                    }
+                    .back-icon a {
+                        text-decoration: none;
+                        color: #000000;
+                        font-size: 24px;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="back-icon">
+                    <a href="./search.html"><i class="fas fa-arrow-left"></i> Back to Search</a>
+                </div>
+                <h1 style="text-align: center;">Order List</h1>                
+                <table border="1">
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Order Date</th>
+                        <th>Total Amount</th>
+                        <th>Order Status</th>
+                        <th>Payment Method</th>
+                        <th>Image</th>
+                    </tr>
+                    <tbody>
+                        ${tableRowsHTML}
+                    </tbody>
+                </table>
+            </body>
+            </html>
+        `);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Failed to search orders');
+    }    
+})
+
+app.get('/read/for/update', async (req, res) => {
+    const orderID = req.query.orderID;
+    const query = { orderID: { $regex: orderID, $options: 'i' } };
+    
+    try {
+        const ordersCollection = mongodbModule.dbCollection;
+        const editOrder = await ordersCollection.findOne(query);
+
+        const queryParams = querystring.stringify(editOrder);
+        res.redirect(`/pages/update.html?${queryParams}`);
+    } catch (err) {
+        console.error('Error: ', err);
+        res.status(500).send('Internal server error');
     }
 });
 
-app.delete('/api/delete/:orderID', (req, res) => {
-    const orderID = req.params.orderID;
-    // Xóa đơn hàng với ID đã cho
-    const orderIndex = orders.findIndex(order => order.orderID === orderID);
-    if (orderIndex !== -1) {
-        orders.splice(orderIndex, 1);
-        res.send(`Đã xóa đơn hàng có ID = ${orderID} thành công.`);
-    } else {
-        res.send(`Không tìm thấy đơn hàng có ID = ${orderID}.`);
+app.put('/update', async (req, res) => {
+    try {
+        const newOrder = req.body;
+        const orderID = newOrder.orderID;
+
+        const ordersCollection = mongodbModule.dbCollection;
+        await ordersCollection.updateOne({ orderID: orderID }, {
+            $set: {
+                orderDate: newOrder.orderDate,
+                totalAmount: newOrder.totalAmount,
+                orderStatus: newOrder.orderStatus,
+                paymentMethod: newOrder.paymentMethod
+            }
+        });
+
+        const content = `Order ID = ${newOrder.orderID} updated successfully !!!`;
+        res.json({ message: content }); // Response to fetch()
+    } catch (err) {
+        console.error('Error: ', err);
+        res.status(500).send('Internal server error');
     }
 });
 
 
-app.put("/api/orders/:orderID", (req, res) => {
-    const orderIdToUpdate = req.params.orderID;
-    const { orderDate, totalAmount, orderStatus, paymentMethod, image } = req.body;
-
-    // Tìm và cập nhật đơn hàng có orderID cần cập nhật
-    const indexToUpdate = orders.findIndex(order => order.orderID === orderIdToUpdate);
-    if (indexToUpdate === -1) {
-        res.statusCode = 400;
-        res.json({ message: "Order not found" });
-        return;
-    }
-
-    orders[indexToUpdate].orderDate = orderDate;
-    orders[indexToUpdate].totalAmount = totalAmount;
-    orders[indexToUpdate].orderStatus = orderStatus;
-    orders[indexToUpdate].paymentMethod = paymentMethod;
-    orders[indexToUpdate].image = image;
-
-    res.json({ message: "Order updated successfully" });
-});
-
-app.get('/api/orders/:orderID', (req, res) => {
-    const orderID = req.params.orderID;
-    const foundOrder = orderModule.searchOrders(orderID);
-    if (foundOrder) {
-        res.json(foundOrder);
-    } else {
-        res.status(404).json({ message: 'Order not found' });
-    }
-});
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- public/Javascript/mongoDB.js
+++ public/Javascript/mongoDB.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const { MongoClient } = require("mongodb");
 
 // Replace the uri string with your connection string.
@@ -7,49 +8,49 @@ const client = new MongoClient(url);
 const dbName = 'orders';
 const collectionName = 'order';
 
-async function connectToMongoDB()
-{
+async function connectToMongoDB() {
     try {
         await client.connect();
-        console.log('Connect to MongoDB successfully !!!');
+        //.log('Connect to MongoDB successfully !!!');
     } catch (err) {
-        console.error('Error: ',err);
-    } finally{
-        if(client){
-            await client.connect();
-            console.log('MongoDB connection closed');
-        }
+        throw err;
     }
 }
 
-connectToMongoDB()
+const dbCollection = client.db(dbName).collection(collectionName);
 
-async function findOrder() {
-    try {
-        await client.connect();
-        console.log('Connect to MongoDB successfully !!!')
-        
-        ordersCollection = client.db(dbName).collection(collectionName);
-        
-        //query all
-        const Order = await ordersCollection.find().toArray();
-        Order.forEach(order => {
-            console.log('orderID:', order.orderID);
-            console.log('name:', order.orderDate);
-            console.log('totalAmount:', order.totalAmount);
-            console.log('orderStatus:', order.orderStatus);
-            console.log('paymentMethod:', order.paymentMethod);
-            console.log('image:', order.image);
-            console.log('----------------------------------');
-        });
-    } catch (err) {
-        console.error('Error:', err);
-    } finally {
-        if (client) {
-            await client.close();
-            console.log('MongoDB connection closed');
-        }
+
+async function closeMongoDBConnection() {
+    if (client) {
+        await client.close()
+            .then(() => {
+                console.log('Disconnected from MongoDB');
+                process.exit(0);
+            })
+            .catch((error) => {
+                console.error('Failed to disconnect from MongoDB', error);
+                process.exit(1);
+            })
+    } else {
+        process.exit(0)
     }
+}
+
+const orders = [
+    { "orderID": "OD20240021", "orderDate": "2024-02-13", "totalAmount": 220.00, "orderStatus": "Processing", "paymentMethod": "Credit Card", "image": "14.jpg" },
+    { "orderID": "OD20240022", "orderDate": "2024-02-14", "totalAmount": 80.75, "orderStatus": "Pending", "paymentMethod": "Cash on Delivery", "image": "15.jpg" },
+
+];
+
+
+async function findDocuments(query) {
+    const documents = await dbCollection.find(query).toArray();
+    return documents;
+}
+
+async function findOrderList() {
+    const documents = await dbCollection.find().toArray();
+    return documents;
 }
 
 async function addManyOrders(orders) {
@@ -72,101 +73,132 @@ async function addManyOrders(orders) {
     }
 }
 
-// Dữ liệu orders để thêm vào
-const orders = [
-    { "orderID": "OD20240021", "orderDate": "2024-02-13", "totalAmount": 220.00, "orderStatus": "Processing", "paymentMethod": "Credit Card", "image": "14.jpg" },
-    { "orderID": "OD20240022", "orderDate": "2024-02-14", "totalAmount": 80.75, "orderStatus": "Pending", "paymentMethod": "Cash on Delivery", "image": "15.jpg" },
-    // Thêm dữ liệu tiếp theo tại đây nếu cần
-];
-
-async function deleteOrder(orderID) {
+async function outputDB() {
     try {
-        await client.connect();
-        console.log('Connected to MongoDB successfully !!!');
-        
-        ordersCollection = client.db(dbName).collection(collectionName);
-        
-        const result = await ordersCollection.deleteOne({ orderID: orderID });
-        
-        if (result.deletedCount === 1) {
-            console.log('Order with ID', orderID, 'deleted successfully');
-        } else {
-            console.log('Order with ID', orderID, 'not found');
-        }
-    } catch (err) {
-        console.error('Error:', err);
-    } finally {
-        if (client) {
-            await client.close();
-            console.log('MongoDB connection closed');
-        }
+        let tableRowsHTML = '';
+
+        orders.forEach(order => {
+            tableRowsHTML += `
+                <tr>
+                    <td>${order.orderID}</td>
+                    <td>${order.orderDate}</td>
+                    <td>$${order.totalAmount}</td>
+                    <td>${order.orderStatus}</td>
+                    <td>${order.paymentMethod}</td>
+                    <td><img src="/images/${order.image}" alt="Order Image"></td>
+                </tr>
+            `;
+        });
+
+        return `
+            <html>
+            <head>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        margin: 0;
+                        padding: 0;
+                        background-color: #f4f4f4;
+                    }
+                    table {
+                        width: 100%;
+                        border-collapse: collapse;
+                        background-color: #fff;
+                        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+                    }
+                    th, td {
+                        padding: 12px 15px;
+                        text-align: left;
+                    }
+                    th {
+                        background-color: #007bff;
+                        color: #fff;
+                    }
+                    tr:nth-child(even) {
+                        background-color: #f2f2f2;
+                    }
+                    img {
+                        max-width: 100px;
+                        height: auto;
+                        display: block;
+                        margin: 0 auto;
+                    }
+                    .back-icon {
+                        text-align: left;
+                        margin-bottom: 20px;
+                    }
+                    .back-icon a {
+                        text-decoration: none;
+                        color: #000000;
+                        font-size: 24px;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="back-icon">
+                    <a href="index.html"><i class="fas fa-arrow-left"></i> Back to Home</a>
+                </div>
+                <h1 style="text-align: center;">Order List</h1>                
+                <table border="1">
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Order Date</th>
+                        <th>Total Amount</th>
+                        <th>Order Status</th>
+                        <th>Payment Method</th>
+                        <th>Image</th>
+                    </tr>
+                    ${tableRowsHTML}
+                </table>
+            </body>
+            </html>
+        `;
+    } catch (error) {
+        console.error('Error:', error);
+        return 'Failed to generate order list';
     }
 }
 
-// deleteOrder("OD20240003")
-
-async function updateOrder(orderID, updatedData) {
+async function getOrderById(orderID) {
+    const client = new MongoClient(url);
     try {
         await client.connect();
-        console.log('Connected to MongoorderIDToUpdateDB successfully !!!');
-        
-        ordersCollection = client.db(dbName).collection(collectionName);
-        
-        const result = await ordersCollection.updateOne(
-            { orderID: orderID },
-            { $set: updatedData }
-        );
-        
-        if (result.modifiedCount === 1) {
-            console.log('Order with ID', orderID, 'updated successfully');
-        } else {
-            console.log('Order with ID', orderID, 'not found');
-        }
-    } catch (err) {
-        console.error('Error:', err);
-    } finally {
-        if (client) {
-            await client.close();
-            console.log('MongoDB connection closed');
-        }
-    }
-}
-
-async function updateManyOrders(key, updatedData) {
-    try {
-        await client.connect();
-        console.log('Connect to MongoDB successfully!!!');
-        
-        // Select collection
+        console.log('Connected to MongoDB successfully!!!');
         const ordersCollection = client.db(dbName).collection(collectionName);
-
-        // Thực hiện cập nhật
-        const update = await ordersCollection.updateMany(
-            { orderStatus: { $regex: new RegExp(key, 'i') } }, // Điều kiện tìm kiếm
-            { $set: { orderStatus: updatedData } } // Dữ liệu cập nhật
-        );
-
-        // Kiểm tra xem có bản ghi nào được cập nhật không
-        if (update.modifiedCount === 0) {
-            throw new Error('Order not found to update');
-        }
-        console.log('Order updated successfully!!!');
+        const query = { orderID: { $regex: orderID, $options: 'i' } };
+        const editOrder = await ordersCollection.findOne(query);
+        return editOrder;
     } catch (err) {
-        console.log('Error:', err);
+        console.error('Error in getOrderById:', err);
+        throw err;
     } finally {
-        // Đảm bảo đóng kết nối sau khi hoàn thành
-        if (client) {
-            await client.close();
-            console.log('MongoDB connection closed');
-        }
+        await client.close();
+    }
+}
+
+
+async function getnextIDNumber(client, dbName, collectionName) {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+
+    const lastOrder = await collection.find().sort({ orderID: -1 }).limit(1).toArray();
+
+    if (lastOrder.length === 0) {
+        return 1;
+    } else {
+        const lastID = lastOrder[0].orderID;
+        const nextIDNumber = parseInt(lastID.replace("OD202400", "")) + 1;
+        return nextIDNumber;
     }
 }
 
 module.exports = {
     connectToMongoDB,
-    findOrder,
+    closeMongoDBConnection,
+    findDocuments,
     addManyOrders,
-    deleteOrder,
-    updateOrder,
-    updateManyOrders
+    outputDB,
+    getOrderById,
+    dbCollection,
+    findOrderList
 }
